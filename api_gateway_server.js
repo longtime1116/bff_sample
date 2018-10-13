6aa7246195914b3cd14dfafdfebba411f4a3b900
longtime1116 <longtime1116@gmail.com>
2018-10-13 16:19:02 +0900

    渡した user_id に見合ったものが返るように修正

--- api_gateway_server.js
+++ api_gateway_server.js
@@ -1,36 +1,47 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 
 // GraphQL schema
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
-    top: Root
-    user: User
-    item: Item
+    top(id: ID!): Root
   },
-  type Item {
-    user_id: Int
+  type Todo {
+    user_id: ID!
     title: String
   },
   type User {
-    id: Int
+    id: ID!
     name: String
   },
   type Root {
     user: User
-    items: [Item]
-  }
+    todos: [Todo]
+  },
 `);
 
-var root = {
-  top: { user: { id: 1, name: "Tom" }, items: [ { user_id: 1, title: "example1"  }, { user_id: 1, title: "example2"  } ]  }
+const top_data = user_id => {
+  return {
+    top : {
+      user: { id: user_id, name: "Tom" },
+      todos: [ { user_id: user_id, title: "example1"  }, { user_id: user_id, title: "example2"  } ]
+    }
+  }
 };
 
-var app = express();
-app.use('/graphql', express_graphql({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
+const parse_user_id = params  => {
+  return parseInt((params.query.replace(/\s+/g, "").match(/top\(id:([0-9])+\)/) || [])[1]) || undefined
+}
+
+const app = express();
+app.use('/graphql', express_graphql((request, response, graphQLParams) => {
+  return {
+    schema: schema,
+    rootValue: top_data(parse_user_id(graphQLParams)),
+    graphiql: true
+  }
 }));
+
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
